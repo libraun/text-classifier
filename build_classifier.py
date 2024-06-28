443d6f57b2a5436c4a4483760305c24accb5c970
libraun <jetbraun@gmail.com>
2024-06-28 18:28:10 -0500

    Bug fixes; moved training and eval functions into module; renamed class

--- build_classifier.py
+++ build_classifier.py
@@ -8,11 +8,13 @@ import os
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+
+from typing import List, Tuple
 #from torch.nn.utils.rnn import pad_sequence
 
 import torch.optim as optim
 
-import text_sentiment_transformer
+from text_sentiment_classifier import TextSentimentClassifier
 import text_utils
 
 from text_tensor_builder import TextTensorBuilder
@@ -32,7 +34,6 @@ def load_data(json_object: dict, trg_len: int = 10000):
             break
 
         all_message_data = conversation["content"]
-
         for message_data in all_message_data:
 
             if msg_count == trg_len:
@@ -63,65 +64,16 @@ def save_tensor(data, path, collate_fn):
     )
     torch.save(data_iter, path)
 
-def create_vocab(data):
-    query_data = list()
-    # i dislike this substantially but im tired. why did i even decide to do literally any of this in the first place theres literally already a chatgpt
-    for convo in data:
-        for agent in convo:
-            for msg in agent:
-                query_data.append(msg[0])
+def create_vocab(data: List[Tuple[str, str]]):
+    query_data = [pair[0] for pair in data]
+
     tensor_builder = TextTensorBuilder(EMBED_DIM, query_data)
     return tensor_builder
 
-def create_sentiment_tensor(sentiment, all_sentiments) -> int:
+def get_sentiment_label(sentiment: str, all_sentiments: List[str]) -> int:
 
     return all_sentiments.index(sentiment)
 
-
-def evaluate():
-    global model, valid_tensor
-    model.eval()
-    epoch_loss = 0
-    with torch.no_grad():
-        for src, trg, label in valid_tensor:
-          #  src,trg = src.to(device), trg.to(device)
-            output = model(src,label)
-            output = output.view(-1, trg.shape[-1])
-
-            loss = criterion(output, trg)
-            epoch_loss += loss.item()
-
-    return epoch_loss / len(valid_tensor)
-
-def train(num_epochs: int):
-
-    global model, train_tensor, optimizer, criterion, en_vocab
-
-    train_loss_values = []
-    validation_loss_values = []
-    for _ in range(num_epochs):
-        epoch_loss = 0
-        model.train() # Set training to true
-        for src, trg, label in train_tensor:
-            optimizer.zero_grad()
-
-            output = model(src, label)
-            output = output.view(-1,trg.shape[-1])
-
-            loss = criterion(output,trg)
-            loss.backward()
-
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 0.1)
-            optimizer.step()
-
-            epoch_loss += loss.item()
-        # Add mean loss value as epoch loss.
-        epoch_loss = epoch_loss / len(train_tensor)
-        val_loss = evaluate()
-
-        train_loss_values.append(epoch_loss)
-        validation_loss_values.append(val_loss)
-    return train_loss_values, validation_loss_values
 
 def collate_sentiment_data(data_batch):
     
@@ -135,34 +87,26 @@ def collate_sentiment_data(data_batch):
     out_batch = torch.tensor(out_batch, dtype=torch.int64)
     return in_batch,out_batch,offsets
 
-def load_tensors(path):
-
-    with open(path, "rb") as f:
-
-        buffer = io.BytesIO(f.read())
-    dst = torch.load(buffer)
-    return dst
-
 if __name__ == "__main__":
 
-    trg_data_len = int(sys.argv[1])
-    data_len_str = str(trg_data_len)
-
-    dataset_path = sys.argv[2]
+    dataset_path = sys.argv[1]
 
     if not os.path.isfile(dataset_path):
         print("ERROR: Dataset filepath could not be found!")
         exit(EXIT_FAILURE)
 
+    trg_data_len = int(sys.argv[2])
+    data_len_str = str(trg_data_len)
+
     with open(dataset_path, "r") as f:
         json_data = json.load(f)
         
-    message_data, sentiment_ids = load_data(json_data) 
-    print(len(sentiment_ids))
+    message_sentiment_pairs, sentiment_ids = load_data(json_data) 
 
+    messages = [p[0] for p in message_sentiment_pairs]
     sentiment_ids = list(sentiment_ids)
     
-    tensor_builder = create_vocab(message_data)
+    tensor_builder = create_vocab(messages)
 
     tokenizer = tensor_builder.tokenizer
     en_vocab = tensor_builder.get_vocab()
@@ -171,23 +115,21 @@ if __name__ == "__main__":
         pickler = pickle.Pickler(f)
         pickler.dump(en_vocab)
 
-    data_len_str = str(trg_data_len) # Used for printing progress
-
     count = 0 # Record progress
     msg_tensors, sentiment_tensors = list(), list()
-    for pair in message_data:
+    for pair in message_sentiment_pairs:
             
         input_msg = pair[0]
         input_sentiment = pair[1]
 
-        sentiment_tensor = create_sentiment_tensor(pair[1], sentiment_ids)
+        sentiment_tensor = get_sentiment_label(pair[1], sentiment_ids)
 
         msg_tensor = tensor_builder.convert_text_to_tensor(
             input_msg, tokenize=True )
 
         sentiment_tensors.append((msg_tensor, sentiment_tensor))
 
-    # Make train/validation/test splits
+    # "This can't be how this done" - the guy trying to reinvent the wheel
     train_end_idx = math.floor(len(sentiment_tensors) * 0.8)
     valid_end_idx = math.floor(len(sentiment_tensors) * 0.1) + train_end_idx
 
@@ -199,14 +141,11 @@ if __name__ == "__main__":
     valid_tensor = DataLoader(valid_split, batch_size=BATCH_SIZE, shuffle=False,collate_fn=collate_sentiment_data)
     test_tensor = DataLoader(test_split, batch_size=BATCH_SIZE, shuffle=False, collate_fn=collate_sentiment_data)
 
-    model = text_sentiment_transformer.TextSentimentTransformer(
-        len(en_vocab), 8, EMBED_DIM, en_vocab["<pad>"])
+    model = TextSentimentClassifier(len(en_vocab), , EMBED_DIM, en_vocab["<pad>"])
 
     optimizer = optim.Adam(model.parameters())
     criterion = nn.CrossEntropyLoss()
 
-    train_loss_vals, valid_loss_vals = train(10)
-    for loss in train_loss_vals:
-        print(loss)
+    train_loss_vals, valid_loss_vals = model.train_model(10)
 
     exit(EXIT_SUCCESS)
--- text_sentiment_classifier.py
+++ text_sentiment_classifier.py
@@ -1,25 +1,38 @@
+import torch
 import torch.nn as nn
+import torch.optim as optim
 
-
-class TextSentimentTransformer(nn.Module):
+class TextSentimentClassifier(nn.Module):
 
     def __init__(self, input_features: int, 
                  output_features: int, 
                  embed_dim: int, 
                  padding_idx: int,
-                 hidden_dim: int = 256):
+                 hidden_dim: int = 256,
+                 optimizer: str = "adam", 
+                 criterion = nn.CrossEntropyLoss(),
+                 lr: float = 0.001):
         
         super().__init__()
         
         self.embedding = nn.EmbeddingBag(input_features, embed_dim, 
-                                         padding_idx=padding_idx, sparse=False)
+                                         padding_idx=padding_idx, 
+                                         sparse=False)
+        
         
         self.lin1 = nn.Linear(embed_dim, hidden_dim)
         self.lin2 = nn.Linear(hidden_dim, hidden_dim)
 
-        self.fc_out = nn.Linear(256, output_features)
+        self.fc_out = nn.Linear(hidden_dim, output_features)
 
         self.softmax = nn.Softmax()
+
+        if optimizer == "adam":
+            self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        elif optimizer == "mse":
+            self.optimizer = optim.MSELoss(self.parameters(), lr=lr)
+
+        self.criterion = criterion
 
     '''
         self.init_weights()
@@ -41,5 +54,47 @@ class TextSentimentTransformer(nn.Module):
         x = self.fc_out(x)
         return x
         
+    def evaluate_model(self, valid_tensor):
+        self.eval()
+        epoch_loss = 0
+        with torch.no_grad():
+            for src, trg, label in valid_tensor:
+            #  src,trg = src.to(device), trg.to(device)
+                output = self.forward(src,label)
+                output = output.view(-1, trg.shape[-1])
 
-        
+                loss = self.criterion(output, trg)
+                epoch_loss += loss.item()
+
+        return epoch_loss / len(valid_tensor)
+
+    def train_model(self, train_data, 
+                    valid_data, 
+                    num_epochs: int):
+
+        train_loss_values = []
+        validation_loss_values = []
+        for _ in range(num_epochs):
+            epoch_loss = 0
+            self.train() # Set training to true
+            for src, trg, label in train_data:
+                self.optimizer.zero_grad()
+
+                output = self.model(src, label)
+                output = output.view(-1,trg.shape[-1])
+
+                loss = self.criterion(output,trg)
+                loss.backward()
+
+                torch.nn.utils.clip_grad_norm_(self.parameters(), 0.1)
+                self.optimizer.step()
+
+                epoch_loss += loss.item()
+            # Add mean loss value as epoch loss.
+            epoch_loss = epoch_loss / len(train_data)
+            val_loss = self.evaluate(valid_data)
+
+            train_loss_values.append(epoch_loss)
+            validation_loss_values.append(val_loss)
+        return train_loss_values, validation_loss_values
+            
