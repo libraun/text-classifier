88c733288910e6a6bf2a46f2a06bf3946b794175
libraun <jetbraun@gmail.com>
2024-06-29 00:20:33 -0500

    Added class methods 'save_to_path(pk_path)' and 'load_from_path(pk_path)', which loads a TextTensorBuilder instance from a .pickle file.

--- text_tensor_builder.py
+++ text_tensor_builder.py
@@ -1,6 +1,6 @@
-import torch
-import torch.nn as nn
+import pickle
 
+import torch
 import torchtext
 torchtext.disable_torchtext_deprecation_warning()
 
@@ -11,8 +11,7 @@ from torchtext.data.utils import get_tokenizer
 
 class TextTensorBuilder:
 
-    def __init__(self, 
-                 embedding_dim: int, 
+    def __init__(self,  
                  data: List[str],
                  tokenizer: str="spacy",
                  tokenizer_lang: str="en_core_web_sm",
@@ -23,28 +22,47 @@ class TextTensorBuilder:
             specials.insert(0, default_token)
 
         self.tokenizer = get_tokenizer(tokenizer,tokenizer_lang)
-        self.en_vocab = self.__build_vocab__(data, specials)
+        self.lang_vocab = self.__build_vocab__(data, specials)
 
-        self.en_vocab.set_default_index(self.en_vocab[default_token])
+        self.lang_vocab.set_default_index(self.lang_vocab[default_token])
 
-        self.embedding = nn.Embedding(len(self.en_vocab), embedding_dim)
+    @staticmethod
+    def load_from_path(self, pk_path: str):
+        
+        with open(pickle_file_path, "rb") as f:
+
+            state_dict = pickle.load(f)
+
+        self.__dict__ = state_dict
+        return self
+    
+    
+    def save_to_path(self, pk_path):
+
+        with open(pk_path, "wb+") as f:
+            pickler = pickle.Pickler(f)
+            pickler.dump(self.__dict__)
     
     def convert_text_to_tensor(self, 
                                doc: str | List[str], 
                                tokenize: bool=True ) -> torch.Tensor: 
         
         tokens = doc if not tokenize else self.tokenizer(doc)
-        text_tensor = [self.en_vocab[token] for token in tokens]
+        text_tensor = [self.lang_vocab[token] for token in tokens]
         text_tensor = torch.tensor(text_tensor, dtype=torch.int64)
 
         return text_tensor
     
-    def get_vocab(self):
-        return self.en_vocab
+    def serialize(self, path):
+
+        with open(path, "wb+") as f:
+            pickler = pickle.Pickler(f)
+            pickler.dump(self.__dict__)
 
     def __build_vocab__(self,
                         corpus: List[str],
                         specials: List[str]):
+        
         counter = Counter()
         for text in corpus:
             tokens = self.tokenizer(text)
